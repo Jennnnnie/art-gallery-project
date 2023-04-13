82dc4298aea7ed44981dd44785515b25112559d2
Jennie <jen.nie.333.chen@gmail.com>
2023-04-13 16:46:36 -0400

    Added homepage component

---
+++ src/components/homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.css";
+
+const Homepage = () => {
+  return (
+    <>
+      <div className="">Hello!</div>
+    </>
+  );
+};
+
+export default Homepage;
