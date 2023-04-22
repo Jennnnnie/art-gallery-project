77e4aef4b22c2a5f9a0b4edfd192abebf2511b0d
Jennie <jen.nie.333.chen@gmail.com>
2023-04-22 17:18:42 -0400

    Added gallery component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import { Header } from "./components";
+import { Gallery } from "./components";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Gallery />
     </div>
   );
 }
