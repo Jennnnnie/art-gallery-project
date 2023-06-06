4f1f49b63394fd30324979fbdda94ba721dd6007
Jen-nie333 <104903826+Jen-nie333@users.noreply.github.com>
2023-06-05 22:49:47 -0400

    Fixed error message

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from "./home/header/Header";
 export { default as Gallery } from "./home/gallery/Gallery";
 export { default as Footer } from "./home/footer/Footer";
 export { default as Location } from "./location/Location";
+export { default as Home } from "./home/Home";
